5d9691c28d6c3f89f0f4997478509c8a36c415a9
Gen_J <15971199009@163.com>
2024-10-15 17:35:32 +0800

    移除多余文件

--- liquor-demo/eslint.config.mjs
+++
@@ -1,6 +0,0 @@
-import antfu from '@antfu/eslint-config'
-
-export default antfu({
-  // eslint 忽略文件清单
-  ignores: []
-})
--- liquor-demo/src/components/test.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-import { reactive, ref } from 'vue'
-</script>
-
-<template>
-  <el-button>About me</el-button>
-</template>
-
-<style lang="scss" scoped></style>
